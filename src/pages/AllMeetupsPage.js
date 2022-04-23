a701a149cbf3fd7d0fe61dc3366719b88dcfef4b
DevlopFE <jcxavidazn@gmail.com>
2022-04-23 11:01:13 +0200

    fetching initial data on showing component

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,17 +1,30 @@
-import MeetupItem from "../components/meetups/MeetupItem";
-import classes from "./../components/meetups/MeetupList.module.css";
+import { useEffect} from "react";
+import MeetupContainer from "../components/meetups/MeetupContainer";
+import { useFetch } from "../util-hooks/useFetch";
+import classes from "../components/meetups/MeetupContainer.module.css";
+import { useMeetup } from '../util-hooks/useMeetup';
 
 
 export default function AllMeetupsPage() {
+  const { MeetupState, setAllMeetups} = useMeetup()
+  const {allMeetups} = MeetupState
+  const { data } = useFetch({
+    url: "/data.json",
+  });
+
+useEffect(() => {
+if(allMeetups.length === 0 && data){
+  setAllMeetups(data)
+}
+}, [data])
+
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul className={classes.list}>
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-      </ul>
+      <div className={classes.list}>
+        <MeetupContainer meetups={allMeetups} />
+      </div>
     </section>
   );
 }
