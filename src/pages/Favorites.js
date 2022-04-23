64695e60a6e07e767bc0ab092ea2a8f52f793e7e
DevlopFE <jcxavidazn@gmail.com>
2022-04-23 11:00:25 +0200

    adding component to show fav list

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,17 @@
+import MeetupContainer from "../components/meetups/MeetupContainer";
+import classes from "../components/meetups/MeetupContainer.module.css";
+import { useMeetup } from '../util-hooks/useMeetup';
+
+
 export default function FavoritesPage() {
+  const { MeetupState} = useMeetup()
+  const {favoritesMeetups} = MeetupState
   return (
     <section>
       <h1>Favorites Page</h1>
+      <div className={classes.list}>
+        <MeetupContainer meetups={favoritesMeetups} />
+      </div>
     </section>
   );
 }
