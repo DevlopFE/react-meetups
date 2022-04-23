aaf008a766ea948fe64b703fc721b11e9ef1e98f
DevlopFE <jcxavidazn@gmail.com>
2022-04-23 16:14:01 +0200

    remove from Main a header and create a Header component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,52 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom';
+import { useMeetup } from '../../util-hooks/useMeetup';
+import classes from "./Main.module.css";
+
+export const Header = () => {
+  const headerRef = useRef(null);
+  const [y, setY] = useState(window.scrollY);
+  const { MeetupState:{totalFavorites}} = useMeetup()
+
+  const handleScroll = (e) => {
+    const header = headerRef.current;
+    const window = e.currentTarget;
+    if (y > window.scrollY) {
+      header.classList.remove(classes.hide);
+    } else if (y < window.scrollY) {
+      header.classList.add(classes.hide);
+    }
+    setY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => handleScroll(e));
+
+    return () => { 
+      window.removeEventListener("scroll", (e) => handleScroll(e));
+    };
+  }, [y]);
+
+
+  return (
+    <header ref={headerRef} className={classes.header} data-test="navigation-header">
+    <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/all-meetups" className={({isActive}) => isActive ? "nav-active": ''} > All Meetups</NavLink>
+          </li>
+          <li>
+            <NavLink to="/form"  className={({isActive}) => isActive ? "nav-active": ''} > Add New Meetup</NavLink>
+          </li>
+          <li>
+            <NavLink to="/favorites"  className={({isActive}) => isActive ? "nav-active": ''} >
+                My Favorites
+                <span className={classes.badge}>{totalFavorites}</span>
+                </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,48 +1,26 @@
 
-import {useState, useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import {
   BrowserRouter,
   Route,
   Routes,
-  Navigate,
-  NavLink,
+  Navigate
 } from 'react-router-dom';
 import AllMeetupsPage from "../../pages/AllMeetupsPage";
 import FavoritesPage from "../../pages/Favorites";
 import NewMeetupPage from "../../pages/NewMeetup";
 import { useMeetup } from '../../util-hooks/useMeetup';
 import Layout from './Layout';
-import classes from "./Main.module.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ScrollToTop from './ScrollToTop';
 import { useFetch } from '../../util-hooks/useFetch';
+import { Header } from './Header';
 
 
 export const Main = () => {
-  const headerRef = useRef(null);
-  const { MeetupState:{totalFavorites, allMeetups}, setAllMeetups} = useMeetup()
-  const [y, setY] = useState(window.scrollY);
+  const { MeetupState:{allMeetups}, setAllMeetups} = useMeetup()
   const { data } = useFetch({ url: "/data.json" });
-  
-  const handleScroll = (e) => {
-    const header = headerRef.current;
-    const window = e.currentTarget;
-    if (y > window.scrollY) {
-      header.classList.remove(classes.hide);
-    } else if (y < window.scrollY) {
-      header.classList.add(classes.hide);
-    }
-    setY(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", (e) => handleScroll(e));
-
-    return () => { 
-      window.removeEventListener("scroll", (e) => handleScroll(e));
-    };
-  }, [y]);
 
   useEffect(() => {
   if(allMeetups.length === 0 && data){
@@ -53,28 +31,11 @@ export const Main = () => {
   return (
     <>
     <BrowserRouter>
-      <header ref={headerRef} className={classes.header} data-test="navigation-header">
-      <div className={classes.logo}>React Meetups</div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/all-meetups" className={({isActive}) => isActive ? "nav-active": ''} > All Meetups</NavLink>
-            </li>
-            <li>
-              <NavLink to="/form"  className={({isActive}) => isActive ? "nav-active": ''} > Add New Meetup</NavLink>
-            </li>
-            <li>
-              <NavLink to="/favorites"  className={({isActive}) => isActive ? "nav-active": ''} >
-                  My Favorites
-                  <span className={classes.badge}>{totalFavorites}</span>
-                  </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+
+      <Header />
+          <ScrollToTop>
 
         <Layout>
-          <ScrollToTop>
             <Routes>
               <Route path="/" element={<Navigate to='/all-meetups' replace />} />
               <Route path="/all-meetups" element={<AllMeetupsPage />} />
@@ -82,9 +43,9 @@ export const Main = () => {
               <Route path="/favorites" element={<FavoritesPage />} />
               <Route path="/*" element={<Navigate to='/all-meetups' replace />} />
             </Routes>
-          </ScrollToTop>
         </Layout>
 
+          </ScrollToTop>
     </BrowserRouter>
     <ToastContainer />
     </>
