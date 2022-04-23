527223b61dda765415e04e031a75b8e074931b70
DevlopFE <jcxavidazn@gmail.com>
2022-04-23 11:03:23 +0200

    adding logic on form with validator and toasts to show info to user

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,34 +1,60 @@
+import { initialMeetup } from "../../context/provider";
+import { useMeetup } from "../../util-hooks/useMeetup";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
+import { toast } from 'react-toastify';
+import { meetupSchema } from "../../utils/validators";
+import { createId } from "../../utils/tools";
+
 
 export default function NewMeetupForm() {
+  const {MeetupState, handleChange, setNewMeetup, addNewMeetupOnList} = useMeetup()
+  const {meetup} = MeetupState
   function submitHandler(event) {
     event.preventDefault();
+    const isValid = meetupSchema.validate(meetup)
+    isValid.then((res) => {
+      const meetupWithId = {...meetup}
+      meetupWithId.id = createId(MeetupState.allMeetups)
+      addNewMeetupOnList(meetupWithId)
+      setNewMeetup(initialMeetup)
+      toast.success("Meetup added!");
+    })
+    .catch((err) => {
+      toast.error(err.message);
+    })
   }
 
+  const handleChangeForm = (event) => {
+    const { id, value } = event.target;
+    handleChange(id, value);
+  };
+
   return (
+    <>
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input value={meetup.title} type="text"  id="title" onChange={(e) => handleChangeForm(e)} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input value={meetup.image} type="url"  id="image" onChange={(e) => handleChangeForm(e)} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input value={meetup.address} type="text"  id="address" onChange={(e) => handleChangeForm(e)} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea value={meetup.description} id="description"  rows="5" onChange={(e) => handleChangeForm(e)}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
         </div>
       </form>
     </Card>
+    </> 
   );
 }
